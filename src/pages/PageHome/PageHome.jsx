67608c1e0a22120895526271ee1e3d8218e3d7eb
JeromeTGH <jerome.tomski@gmx.fr>
2023-07-01 21:50:22 +0200

    Préparation de la procédure de recherche, dans la page d'accueil

--- src/pages/PageHome/PageHome.jsx
+++ src/pages/PageHome/PageHome.jsx
@@ -5,12 +5,17 @@ import SearchIcon from '@mui/icons-material/Search';
 
 const PageHome = () => {
 
-    const [ searchFieldValue, setSearchFieldValue ] = useState(null);
+    const [ searchFieldValue, setSearchFieldValue ] = useState('');
     const [ errorMessage, setErrorMessage ] = useState('');
 
     const handleBtnClick = (e) => {
         e.preventDefault();
         setErrorMessage('');
+
+        if(searchFieldValue === '') {
+            setErrorMessage('No search value entered');
+            return;    
+        }
 
 
         console.log("searchFieldValue", searchFieldValue);
@@ -21,7 +26,13 @@ const PageHome = () => {
         <div className={styles.homepage}>
             <h1>Terra Classic Finder</h1>
             <form className={styles.searchBar}>
-                <input type='search' placeholder='Account address / Block number / Transaction hash' autoFocus onChange={(e) => setSearchFieldValue(e.target.value)} />
+                <input
+                    type='search'
+                    placeholder='Account address / Block number / Transaction hash'
+                    autoFocus
+                    onChange={(e) => setSearchFieldValue(e.target.value)}
+                    value={searchFieldValue}
+                />
                 <button onClick={(e) => {handleBtnClick(e)}}><SearchIcon /></button>
             </form>
             <div className={styles.message}>{errorMessage}</div>
