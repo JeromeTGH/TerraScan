da4132b9e65a701d27c41067d321a145ec7d6cb7
JeromeTGH <jerome.tomski@gmx.fr>
2023-07-04 11:39:48 +0200

    Ajout d'un breakpoint css, pour le passage d'un 'grand' écran à un 'petit'

--- src/pages/PageHome/PageHome.jsx
+++ src/pages/PageHome/PageHome.jsx
@@ -27,17 +27,18 @@ const PageHome = () => {
         <div className={styles.homepage}>
             <div className={styles.texts}>
                 <h1>Terra Classic Finder</h1>
-                <p className='comment'>Enter an <span className={styles.highlighted}>Account&nbsp;address</span>, a <span className={styles.highlighted}>Block&nbsp;number</span>, or a <span className={styles.highlighted}>Transaction&nbsp;hash</span> below</p>
+                <p className='comment'>Enter an <span className={styles.highlighted}>Account&nbsp;address</span>, a <span className={styles.highlighted}>Block&nbsp;number</span>, or a <span className={styles.highlighted}>Transaction&nbsp;hash</span> below, for&nbsp;a&nbsp;search (exact&nbsp;match)</p>
             </div>
             <form className={styles.searchBar}>
                 <input
                     type='search'
                     placeholder='Search for ...'
+                    className='homesearch'
                     autoFocus
                     onChange={(e) => setSearchFieldValue(e.target.value)}
                     value={searchFieldValue}
                 />
-                <button onClick={(e) => {handleBtnClick(e)}}><SearchIcon /></button>
+                <button className='homesearch' onClick={(e) => {handleBtnClick(e)}}><SearchIcon /></button>
             </form>
             <p className={styles.message}>{errorMessage}</p>
         </div>
