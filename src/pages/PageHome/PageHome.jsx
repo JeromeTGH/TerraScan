3cf2f6aba63390d3ddea894ed31d7bf1ed4a1c44
JeromeTGH <jerome.tomski@gmx.fr>
2023-07-04 19:41:14 +0200

    Remise en page de la page Home

--- src/pages/PageHome/PageHome.jsx
+++ src/pages/PageHome/PageHome.jsx
@@ -37,18 +37,21 @@ const PageHome = () => {
                 <h1>Terra Classic Finder</h1>
                 <p className='comment'>Enter an <span className={styles.highlighted}>Account&nbsp;address</span>, a <span className={styles.highlighted}>Block&nbsp;number</span>, or a <span className={styles.highlighted}>Transaction&nbsp;hash</span> below, for&nbsp;a&nbsp;search (exact&nbsp;match)</p>
             </div>
-            <form className={styles.searchBar}>
-                <input
-                    type='search'
-                    placeholder='Search for ...'
-                    className='homesearch'
-                    autoFocus
-                    onChange={(e) => setSearchFieldValue(e.target.value)}
-                    value={searchFieldValue}
-                />
-                <button className='homesearch' onClick={(e) => {handleBtnClick(e)}}><SearchIcon /></button>
-            </form>
-            <p className={styles.message}>{errorMessage}</p>
+            <div className={styles.searchBar}>
+                <form>
+                    <input
+                        type='search'
+                        placeholder='Search for ...'
+                        className='homesearch'
+                        autoFocus
+                        onChange={(e) => setSearchFieldValue(e.target.value)}
+                        value={searchFieldValue}
+                    />
+                    <button className='homesearch' onClick={(e) => {handleBtnClick(e)}}><SearchIcon /></button>
+                </form>
+                <p className={styles.message}>{errorMessage}</p>
+            </div>
+            <div className={'comment ' + styles.cmtbtm}>Using Terra Classic LCD (colombus-5)</div>
         </div>
     );
 };
