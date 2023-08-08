76d0c1cbcf854551e245c5bfb8b91a7d1464b42e
JeromeTGH <jerome.tomski@gmx.fr>
2023-08-08 19:15:38 +0200

    Ajout from/to au bloc tx de la page block

--- src/pages/PageBlock/BlockTransactions.jsx
+++ src/pages/PageBlock/BlockTransactions.jsx
@@ -31,14 +31,50 @@ const BlockTransactions = (props) => {
                     <thead>
                         <tr>
                             <th>Tx Hash</th>
+                            <th>Code</th>
                             <th>Tx Type</th>
+                            <th>From</th>
+                            <th>To</th>
                         </tr>
                     </thead>
                             <tbody>
                         {tableBlockTransactions.map((valeur, clef) => {
                             return <tr key={clef}>
                                 <td><Link to={"/transactions/" + valeur[0]}>{valeur[0]}</Link></td>
-                                <td>{valeur[2]}</td>
+                                <td>{valeur[1] === 0 ? <span className='succes'>OK</span> : <span className='erreur'>FAILURE</span>}</td>
+                                <td>{valeur[3]}</td>
+                                <td>
+                                    {valeur[3] === 'MsgSend' ? <Link to={"/accounts/" + valeur[4]}>{valeur[4]}</Link> : null}
+                                    {valeur[3] === 'MsgDelegate' ? <Link to={"/accounts/" + valeur[4]}>{valeur[4]}</Link> : null}
+                                    {valeur[3] === 'MsgUndelegate' ? <Link to={"/validators/" + valeur[4]}>{valeur[6]}</Link> : null}
+                                    {valeur[3] === 'MsgBeginRedelegate' ? <Link to={"/accounts/" + valeur[4]}>{valeur[4]}</Link> : null}
+                                    {valeur[3] === 'MsgVote' ? <Link to={"/accounts/" + valeur[4]}>{valeur[4]}</Link> : null}
+                                    {valeur[3] === 'MsgWithdrawDelegatorReward' ? <Link to={"/validators/" + valeur[4]}>{valeur[6]}</Link> : null}
+                                    {valeur[3] !== 'MsgSend'
+                                        && valeur[3] !== 'MsgDelegate'
+                                        && valeur[3] !== 'MsgUndelegate'
+                                        && valeur[3] !== 'MsgBeginRedelegate'
+                                        && valeur[3] !== 'MsgVote'
+                                        && valeur[3] !== 'MsgWithdrawDelegatorReward'
+                                        ? "--" : null
+                                    }
+                                </td>
+                                <td>
+                                    {valeur[3] === 'MsgSend' ? <Link to={"/accounts/" + valeur[5]}>{valeur[5]}</Link> : null}
+                                    {valeur[3] === 'MsgDelegate' ? <Link to={"/validators/" + valeur[5]}>{valeur[6]}</Link> : null}
+                                    {valeur[3] === 'MsgUndelegate' ? <Link to={"/accounts/" + valeur[5]}>{valeur[5]}</Link> : null}
+                                    {valeur[3] === 'MsgBeginRedelegate' ? <Link to={"/validators/" + valeur[5]}>{valeur[6]}</Link> : null}
+                                    {valeur[3] === 'MsgVote' ? <Link to={"/proposals/" + valeur[5]}>Proposal #{valeur[5]}</Link> : null}
+                                    {valeur[3] === 'MsgWithdrawDelegatorReward' ? <Link to={"/accounts/" + valeur[5]}>{valeur[5]}</Link> : null}
+                                    {valeur[3] !== 'MsgSend'
+                                        && valeur[3] !== 'MsgDelegate'
+                                        && valeur[3] !== 'MsgUndelegate'
+                                        && valeur[3] !== 'MsgBeginRedelegate'
+                                        && valeur[3] !== 'MsgVote'
+                                        && valeur[3] !== 'MsgWithdrawDelegatorReward'
+                                        ? "--" : null
+                                    }                                  
+                                </td>
                             </tr>
                         })}
                     </tbody>
--- src/pages/PageBlock/getTransactions.js
+++ src/pages/PageBlock/getTransactions.js
@@ -1,5 +1,6 @@
 import { chainID, chainLCDurl } from '../../application/AppParams';
 import { LCDClient, hashToHex } from '@terra-money/terra.js';
+import { isValidTerraAddressFormat } from '../../application/AppUtils';
 
 
 export const getTransactions = async (blockNumber) => {
@@ -20,10 +21,12 @@ export const getTransactions = async (blockNumber) => {
         rawBlockInfos.block.data.txs.forEach(element => {
             transactionsInfos.push([
                 hashToHex(element),
+                -1,                     // Code erreur (0 si "ok")
                 0,                      // Nb messages
                 '',                     // Type de message : MsgSend, MsgVote, MsgDelegate, ...
-                '',                     // "From" (dans les cas où çà s'applique, dirons nous !)
-                ''                      // "To" (dans les cas où çà s'applique, dirons nous !)
+                '--',                   // "From" (dans les cas où çà s'applique, dirons nous !)
+                '--',                   // "To" (dans les cas où çà s'applique, dirons nous !)
+                '',                     // Validator's moniker (if there is one, in Msg)
             ])
         })
     } else
@@ -31,9 +34,9 @@ export const getTransactions = async (blockNumber) => {
     
 
     // Récupération des infos sur une transaction donnée ****** TEST
-    // const rawTxInfo = await lcd.tx.txInfo("3355B57C0CDA8F97690D1A40D9A6D28361FF4129B55281ADDA21E43654EC992B").catch(handleError);    
+    // const rawTxInfo = await lcd.tx.txInfo("43AC51F14195A8C0167AAAFE911FDBC78D27FDC4294D602B5ADF986BF0E80AFF").catch(handleError);    
     // if(rawTxInfo) {
-    //     console.log("rawTxInfo", rawTxInfo.tx.body.constructor.name);
+    //     console.log("rawTxInfo", rawTxInfo);
     // } else
     //     return { "erreur": "Failed to fetch [transaction infos] ..." }
 
@@ -41,16 +44,57 @@ export const getTransactions = async (blockNumber) => {
     for(let i=0 ; i<transactionsInfos.length ; i++) {
         const rawTxInfo = await lcd.tx.txInfo(transactionsInfos[i][0]).catch(handleError);
         if(rawTxInfo) {
+            transactionsInfos[i][1] = rawTxInfo.code;
             const nbMessages = rawTxInfo.tx.body.messages.length;
-            transactionsInfos[i][1] = nbMessages;
+            transactionsInfos[i][2] = nbMessages;
             if(nbMessages === 0)
-                transactionsInfos[i][2] = 'Error';
-            else if(nbMessages === 1)
-                transactionsInfos[i][2] = rawTxInfo.tx.body.messages[0].constructor.name;
+                transactionsInfos[i][3] = 'Error';
+            else if(nbMessages === 1) {
+                const msgType = rawTxInfo.tx.body.messages[0].constructor.name;
+                transactionsInfos[i][3] = msgType;
+                if(msgType === 'MsgSend') {
+                    transactionsInfos[i][4] = rawTxInfo.tx.body.messages[0].from_address;
+                    transactionsInfos[i][5] = rawTxInfo.tx.body.messages[0].to_address;
+                } else if(msgType === 'MsgDelegate') {
+                    transactionsInfos[i][4] = rawTxInfo.tx.body.messages[0].delegator_address;
+                    transactionsInfos[i][5] = rawTxInfo.tx.body.messages[0].validator_address;
+                } else if(msgType === 'MsgUndelegate') {
+                    transactionsInfos[i][4] = rawTxInfo.tx.body.messages[0].validator_address;
+                    transactionsInfos[i][5] = rawTxInfo.tx.body.messages[0].delegator_address;
+                } else if(msgType === 'MsgBeginRedelegate') {
+                    transactionsInfos[i][4] = rawTxInfo.tx.body.messages[0].delegator_address;
+                    transactionsInfos[i][5] = rawTxInfo.tx.body.messages[0].validator_dst_address; // from "validator_src_address", en fait
+                } else if(msgType === 'MsgVote') {
+                    transactionsInfos[i][4] = rawTxInfo.tx.body.messages[0].voter;
+                    transactionsInfos[i][5] = rawTxInfo.tx.body.messages[0].proposal_id.toString();
+                } else if(msgType === 'MsgWithdrawDelegatorReward') {
+                    transactionsInfos[i][4] = rawTxInfo.tx.body.messages[0].validator_address;
+                    transactionsInfos[i][5] = rawTxInfo.tx.body.messages[0].delegator_address;
+                }
+            }
             else
-                transactionsInfos[i][2] = 'Multiple (' + nbMessages + ' messages)';
+                transactionsInfos[i][3] = 'Multiple (' + nbMessages + ' messages)';
         } else
             return { "erreur": "Failed to fetch [transaction infos] ..." } 
+    }
+
+    // Parcours de tous les "from", à la recherche des adresses de type "terravaloper1..." (car il s'agit de validateur, et donc, il faut récupérer leur "moniker" aussi)
+    for(let i=0 ; i<transactionsInfos.length ; i++) {
+        if(isValidTerraAddressFormat(transactionsInfos[i][4], 'terravaloper1')) {
+            const rawValidators = await lcd.staking.validator(transactionsInfos[i][4]).catch(handleError);
+            if(rawValidators)
+                transactionsInfos[i][6] = rawValidators.description.moniker;
+            else
+                return { "erreur": "Failed to fetch [validators] ..." } 
+        }
+        if(isValidTerraAddressFormat(transactionsInfos[i][5], 'terravaloper1')) {
+            const rawValidators = await lcd.staking.validator(transactionsInfos[i][5]).catch(handleError);
+            if(rawValidators)
+                transactionsInfos[i][6] = rawValidators.description.moniker;
+            else
+                return { "erreur": "Failed to fetch [validators] ..." } 
+        }
+
     }
 
 
