9032c9690ac1ad01b0bd6bcaf963cc1d93330334
JeromeTGH <jerome.tomski@gmx.fr>
2023-08-14 19:04:08 +0200

    Ajout d'un numéro de version projet, sur la page 'about'

--- src/pages/PageAbout/PageAbout.jsx
+++ src/pages/PageAbout/PageAbout.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import packageJson from '../../../package.json';
 
 const PageAbout = () => {
     return (
@@ -9,6 +10,8 @@ const PageAbout = () => {
             <br />
             <p><u>Project sources</u> : <a href="https://github.com/JeromeTGH/Terra-Scan" target="_blank" rel="noreferrer noopener">https://github.com/JeromeTGH/Terra-Scan</a></p>
             <p><u>Licence</u> : <a href="https://creativecommons.org/licenses/by-nc-nd/4.0/" target="_blank" rel="noreferrer noopener">Creative Commons "BY-NC-ND 4.0"</a></p>
+            <br />
+            <p>Version = {packageJson.version}</p>
             <br />
             <p>Created by <a href="https://twitter.com/jerometomski" target="_blank" rel="noreferrer noopener">Jerome TOMSKI</a>, @2023</p>
             <br />
